29b5815f1f42381476e7f79f1e037ddb95bbe8cb
Øivind <s315695@stud.hioa.no>
2018-09-04 22:19:28 +0200

    Adds JUnit code to test max function

---
+++ src/tests/MaxTests.java
@@ -0,0 +1,44 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.Test;
+
+import code.Oblig1;
+
+class maxTest {
+
+    @Test
+    void emptyArrayTest() {
+        int[] emptyArray = {};
+        assertThrows(NoSuchElementException.class, () -> Oblig1.max(emptyArray));
+    }
+    
+    @Test
+    void arrayWithOneElementTest() {
+        int[] arrayWithOneElement = {1};
+        assertEquals(1, Oblig1.max(arrayWithOneElement));
+    }
+    
+    @Test
+    void arrayPointingToNullTest() {
+        int[] arrayPointingToNull = null;
+        assertThrows(NullPointerException.class, () -> Oblig1.max(arrayPointingToNull));
+    }
+    
+    @Test
+    void negativeNumbersTest() {
+        int[] arrayOfNegativeIntegers = {-20, -15, -31, -1, -19};
+        assertEquals(-1, Oblig1.max(arrayOfNegativeIntegers));
+    }
+    
+    @Test
+    void mixedNegativeAndPositiveIntegersTest() {
+        int[] arrayOfIntegers = {-20, 20, 14, -1, 0, 100};
+        assertEquals(100, Oblig1.max(arrayOfIntegers));
+    }
+
+}
