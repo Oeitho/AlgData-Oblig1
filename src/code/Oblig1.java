918b447cb05b6e5e216ed9265e6ed1200dcb0006
Øivind <s315695@stud.hioa.no>
2018-09-04 22:20:05 +0200

    Adds code including max function

---
+++ src/code/Oblig1.java
@@ -0,0 +1,31 @@
+package code;
+
+import java.util.NoSuchElementException;
+
+public class Oblig1 {
+    
+    private Oblig1() {}
+    
+    public static int max(int[] arrayOfIntegers) {
+        int arrayLength = arrayOfIntegers.length;
+        int lastElementPosition = arrayLength - 1;
+        
+        if (arrayLength < 1)
+            throw new NoSuchElementException("The provided array of integers is empty!");
+        
+        for (int i = 0; i < lastElementPosition; i++) {
+            if (arrayOfIntegers[i] > arrayOfIntegers[i + 1]) {
+                swap(arrayOfIntegers, i, i + 1);
+            }
+        }
+        
+        return arrayOfIntegers[lastElementPosition];
+    }
+    
+    private static void swap(int[] arrayOfIntegers, int indexA, int indexB) {
+        int temporaryHolderValue = arrayOfIntegers[indexA];
+        arrayOfIntegers[indexA] = arrayOfIntegers[indexB];
+        arrayOfIntegers[indexB] = temporaryHolderValue;
+    }
+    
+}
