1f7e9f97c43e24c619485597d10cacb65a0add9e
Øivind <s315695@stud.hioa.no>
2018-09-13 14:08:05 +0200

    Added JUnit tests for stringContains function

---
+++ src/tests/StringContainsTests.java
@@ -0,0 +1,67 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import code.Oblig1;
+
+import org.junit.jupiter.api.Test;
+
+class StringContainsTests {
+
+    @Test
+    void firstStringPointsToNullTest() {
+        String firstString = null;
+        String secondString = "ABBA";
+        assertThrows(NullPointerException.class, () -> Oblig1.stringContains(firstString, secondString));
+    }
+    
+    @Test
+    void secondStringPointsToNullTest() {
+        String firstString = "Abba";
+        String secondString = null;
+        assertThrows(NullPointerException.class, () -> Oblig1.stringContains(firstString, secondString));
+    }
+    
+    @Test
+    void bothStringsPointsToNullTest() {
+        String firstString = null;
+        String secondString = null;
+        assertThrows(NullPointerException.class, () -> Oblig1.stringContains(firstString, secondString));
+    }
+    
+    @Test
+    void firstStringEmptyTest() {
+        String firstString = "";
+        String secondString = "ABBA";
+        assertEquals(true, Oblig1.stringContains(firstString, secondString));
+    }
+    
+    @Test
+    void secondStringEmptyTest() {
+        String firstString = "ABBA";
+        String secondString = "";
+        assertEquals(false, Oblig1.stringContains(firstString, secondString));
+    }
+    
+    @Test
+    void bothStringsEmptyTest() {
+        String firstString = "";
+        String secondString = "";
+        assertEquals(true, Oblig1.stringContains(firstString, secondString));
+    }
+    
+    @Test
+    void secondStringContainsAllCharactersInFirstStringTest() {
+        String firstString = "ABBA";
+        String secondString = "ALIBABA";
+        assertEquals(true, Oblig1.stringContains(firstString, secondString));
+    }
+    
+    @Test
+    void secondStringDoesntContainAllCharactersInFirstStringTest() {
+        String firstString = "ABBA";
+        String secondString = "ACDC";
+        assertEquals(false, Oblig1.stringContains(firstString, secondString));
+    }
+
+}
