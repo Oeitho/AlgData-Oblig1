9eb920b01294d051482aafb6fb2f2f0679057115
Øivind <s315695@stud.hioa.no>
2018-09-13 13:36:47 +0200

    Adds test for threeSmallest function

---
+++ src/tests/ThreeSmallestTests.java
@@ -0,0 +1,55 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
+import code.Oblig1;
+
+import org.junit.jupiter.api.Test;
+
+class ThreeSmallestTests {
+
+    @Test
+    void arrayPointingToNullTest() {
+        int[] arrayPointingToNull = null;
+        assertThrows(NullPointerException.class, () -> Oblig1.threeSmallest(arrayPointingToNull));
+    }
+    
+    @Test
+    void arrayContainingOneNumberTest() {
+        int[] arrayContainingOneNumber = {4};
+        assertThrows(NoSuchElementException.class, () -> Oblig1.threeSmallest(arrayContainingOneNumber));
+    }
+    
+    @Test
+    void arrayContainingTwoNumbersTest() {
+        int[] arrayContainingTwoNumbers = {5, 3};
+        assertThrows(NoSuchElementException.class, () -> Oblig1.threeSmallest(arrayContainingTwoNumbers));
+    }
+    
+    @Test
+    void arrayContainingThreeNumbersTest() {
+        int[] arrayContainingThreeNumbers = {4, 2, 0};
+        assertArrayEquals(new int[] {2, 1, 0}, Oblig1.threeSmallest(arrayContainingThreeNumbers));
+    }
+    
+    @Test
+    void arrayContainingFourNumbersTest() {
+        int[] arrayContainingFourNumbers = {4, 2, 0, 5};
+        assertArrayEquals(new int[] {2, 1, 0}, Oblig1.threeSmallest(arrayContainingFourNumbers));
+    }
+    
+    @Test
+    void arrayContainingNegativeNumbers() {
+        int[] arrayContainingNegativeNumbers = {-2, -4, -7, -1, -5};
+        assertArrayEquals(new int[] {2, 4, 1}, Oblig1.threeSmallest(arrayContainingNegativeNumbers));
+    }
+    
+    @Test
+    void arrayContainingMixedNegativeAndPositiveNumbers() {
+        int[] arrayContainingMixedNegativeAndPositiveNumbers = {-4 , 1, 0, 15, 3};
+        assertArrayEquals(new int[] {0, 2, 1}, Oblig1.threeSmallest(arrayContainingMixedNegativeAndPositiveNumbers));
+    }
+    
+}
